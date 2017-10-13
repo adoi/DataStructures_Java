e877a7b0848c9e22d0b93de861ee4bf97d821182
adoi <adonismurati@gmail.com>
2017-10-13 14:49:35 +0200

    Add new solution to the Compare two linked lists problem

---
+++ Linked Lists/Compare two linked lists/Solution.java
@@ -0,0 +1,21 @@
+//Author: Adonis Murati
+
+/*
+  Compare two linked lists A and B
+  Return 1 if they are identical and 0 if they are not. 
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+int CompareLists(Node headA, Node headB) {
+    // This is a "method-only" submission. 
+    // You only need to complete this method 
+    while(headA != null && headB != null && headA.data == headB.data){
+             headA = headA.next;
+             headB = headB.next;
+    }
+    return (headA == headB) ? 1 : 0;
+  
+}
