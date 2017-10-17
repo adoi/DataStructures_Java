d73f5d57cf9aef45fdaed1b17e235e7879a4808d
adoi <adonismurati@gmail.com>
2017-10-17 13:04:23 +0200

    Add new soltion to the Java Map section

---
+++ Map/Java Map/Solution.java
@@ -0,0 +1,36 @@
+//Author: Adonis Murati
+//The code was not written from scratch. I only made modifications to it.
+
+//Complete this code or write your own from scratch
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+   public static void main(String []argh)
+   {
+		Scanner in = new Scanner(System.in);
+		int n=in.nextInt();
+		in.nextLine();
+		
+		HashMap<String, Integer> hashMap = new HashMap<String, Integer>(); 
+		 
+		for(int i=0;i<n;i++)
+		{
+		   String name=in.nextLine();
+		   int phone=in.nextInt();
+		   hashMap.put(name, phone);
+		   in.nextLine();
+		}
+		while(in.hasNext())
+		{
+		   String s = in.nextLine();
+		   if(hashMap.get(s) != null){
+			   System.out.println(s + "=" + hashMap.get(s));
+		   }else{
+			   System.out.println("Not found");
+		   }
+		}
+		sc.close();
+   }
+}
